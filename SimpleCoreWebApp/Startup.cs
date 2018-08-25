3fe29a80b8aed0f6ef5b5e2fa9e2cb95125b0984
Scott Wade <sc0ttwad3@gmail.com>
2018-08-25 16:05:26 -0400

    Refactor app builder with app.Map and .MapWhen routes

--- SimpleCoreWebApp/Startup.cs
+++ SimpleCoreWebApp/Startup.cs
@@ -12,14 +12,16 @@ namespace SimpleCoreWebApp
 {
   public class Startup
   {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    // This OPTIONAL method gets called by the runtime. 
+    // Use this method to add services to the container.
+    // (For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940 )
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddSingleton<IComponent, ComponentB>();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    // This REQUIRED method gets called by the runtime. 
+    // Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IHostingEnvironment env, IComponent component)
     {
       if (env.IsDevelopment())
@@ -27,38 +29,18 @@ namespace SimpleCoreWebApp
         app.UseDeveloperExceptionPage();
       }
 
-      app.Use(async (context, next) =>
-      {
-        HttpRequest  req = context.Request;
-        HttpResponse rsp = context.Response;
+      app.Map("/foo", config => config.Use(async (context, next) => await context.Response.WriteAsync("FOO")));
 
-        if (req.Path == "/foo")
-        {
-          await rsp.WriteAsync($"Welcome to the Foo");
-        } else
-        {
-          await next();
-        }
-      });
+      app.MapWhen(
+        context =>  context.Request.Method == "POST" &&
+                    context.Request.Path == "/bar",
+        config  =>  config.Use(async (context, next) => await context.Response.WriteAsync("POST to BAR"))
 
-      app.Use(async (context, next) =>
-      {
-        HttpRequest req = context.Request;
-        HttpResponse rsp = context.Response;
-
-        if (req.Path == "/bar")
-        {
-          await rsp.WriteAsync($"Welcome to the Bar");
-        }
-        else
-        {
-          await next();
-        }
-      });
+      );
 
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync($"Name is {component.Name}");
+        await context.Response.WriteAsync($"ROOT\n\nInjected component name: {component.Name}");
       });
     }
   }
