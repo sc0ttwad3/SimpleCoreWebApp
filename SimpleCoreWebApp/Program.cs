0be17869ab77d9eb691c67bb18257926d3452903
Scott Wade <sc0ttwad3@gmail.com>
2018-08-25 15:34:56 -0400

    Repo init, add .gitigore, add files, commit

---
+++ SimpleCoreWebApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleCoreWebApp
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateWebHostBuilder(args).Build()
+                                .Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+               // overrides any inline defined config sections
+               // on the web-host builder
+               .UseStartup<Startup>();
+  }
+
+
+  /*
+   *  Section is for illustrating Dependency Injection features
+   *  built into ASP.NET Core.
+   */
+  public interface IComponent
+  {
+    string Name { get; }
+  }
+
+
+  public class ComponentA
+  {
+    private readonly IComponent _componentB;
+
+    public ComponentA(IComponent componentB)
+    {
+      this._componentB = componentB;
+    }
+  }
+
+
+  public class ComponentB: IComponent
+  {
+    public string Name { get; set; } = nameof(ComponentB);
+  }
+}
